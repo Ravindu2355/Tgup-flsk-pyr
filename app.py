413a81c7d80b2d721cd371da7e71e46a6112ce8f
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-18 10:08:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
+from task_manager import write_task
 import os
 from flask_cors import CORS
 
-opw = os.getenv('opw')
 app = Flask(__name__)
 
 CORS(app)
@@ -10,6 +10,13 @@ CORS(app)
 @app.route('/')
 def hello_world():
     return 'Hello from Koyeb'
+    
+@app.route('/upload', methods=['GET'])
+def upload_video():
+    chat_id = request.args.get('chatid')
+    video_url = request.args.get('url')
+    write_task(chat_id=chat_id, url=video_url)
+    return jsonify({"message": "Video add to task list!"})
 
 
 if __name__ == "__main__":
