39eb7b3f3c70016be0cb7713a3e0004ac6f40fcf
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-19 06:04:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify, request
 from task_manager import write_task
 import os
-from bot import app as tg
-from bot import upload_from_url
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,22 +11,6 @@ CORS(app)
 def hello_world():
     return 'Hello from Koyeb'
     
-@app.route('/upload', methods=['GET'])
-def upload_video():
-    chat_id = request.args.get('chatid')
-    video_url = request.args.get('url')
-    if not chat_id or not video_url:
-        return jsonify({"s":0,"message": "No parameter found!"})
-    try:
-        async def run_upload():
-          async with tg:
-            await upload_from_url(tg, chat_id=chat_id, url=video_url)
-
-    # Run the async function in the event loop
-    asyncio.run(run_upload())
-    
-    return jsonify({"s":1,"message": "Video add to task list!"})
-
 
 if __name__ == "__main__":
     app.run(port=8000)
